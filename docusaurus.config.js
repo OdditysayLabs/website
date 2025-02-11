49aa5ff77b11daa15189212a966685837468d7c9
YY <jaiyalas@gmail.com>
2025-02-11 21:51:43 +0800

    added github pages deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,15 +4,16 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 (module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  title: 'Odditysay Labs',
+  tagline: '<tbw:tagline>',
+  url: 'https://OdditysayLabs.github.com/',
+  baseUrl: '/website/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'OdditysayLabs', // Usually your GitHub org/user name.
+  projectName: 'website', // Usually your repo name.
+  deploymentBranch 'gh-pages',
 
   presets: [
     [
